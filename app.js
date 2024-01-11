ef2d46ab2d94e13ba4727844da8cdf1ee0062218
Tonico <tonicogi16@gmail.com>
2024-01-10 23:16:55 -0500

    proyectito

---
+++ app.js
@@ -0,0 +1,23 @@
+//Aritméticos
+let numero1 = 5;
+let numero2 = 10;
+
+console.log (numero1 + numero2);
+console.log (numero1 * numero2);
+
+//asignacion
+let nombre = 'antony';
+
+console.log (nombre);
+
+//comparacion
+
+console.log (numero1 > numero2); 
+
+//logico
+let edad = 25;
+let licencia = ('si');
+
+const puede_conducir = edad >=18 && licencia == 'si';
+
+console.log (puede_conducir);
